81ce729f15d63f81c0586dd8e3d4de26adda4c79
zhengwei <tanzhengwei143@gmail.com>
2018-03-26 23:56:01 +0800

    Run heuristics on all possible moves and select highest valued move. - Replaced previous completeLines function (not working) with a different implementation - Changed heuristic functions input to use int[][] instead of State

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,17 +1,50 @@
+import java.util.Arrays;
 
 public class PlayerSkeleton {
-
+	public static final int COLS = 10;
+	public static final int ROWS = 21;
+	//indices for legalMoves
+	public static final int ORIENT = 0;
+	public static final int SLOT = 1;
+	
+	// Used for each simulated turn
+	public boolean gameEnded = false;
+	public int rowsCleared;
+	
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		bumpinessHeuristic(s);
-		completeLinesHeuristic(s);
-		aggregateHeightHeuristic(s);
-		return 0;
+		int bestMove = 0;
+		double bestScore = -10000;
+		for (int i = 0; i < legalMoves.length; i++) {
+			double score = 0;
+			gameEnded = false;
+			int[] move = legalMoves[i];
+			int[][] newField = simulateMove(s, move);
+			int bumpiness = bumpinessHeuristic(newField);
+			int completeLines = rowsCleared;
+			int aggregateHeight = aggregateHeightHeuristic(newField);
+			if (!gameEnded) {
+				// TODO: How to weight heuristics?
+				score = completeLines + aggregateHeight + bumpiness;				
+			} else {
+				// TODO: This move results in the game ending, how to handle?
+				score = 0;
+			}
+			if (score > bestScore) {
+				bestMove = i;
+				bestScore = score;
+			}
+		}
+		System.out.println("Best Move: " + Arrays.toString(legalMoves[bestMove]));
+		System.out.println("Best Score: " + bestScore);
+		return bestMove;
 	}
+	
 
-	public int aggregateHeightHeuristic(State s) {
+
+	public int aggregateHeightHeuristic(int[][] field) {
 		int aggregateHeight = 0;
-		int[] heights = getColumnHeights(s);
+		int[] heights = getColumnHeights(field);
 
 		for(int i=0; i<heights.length; i++) {
 			aggregateHeight += heights[i];
@@ -20,37 +53,17 @@ public class PlayerSkeleton {
 		return aggregateHeight;
 	}
 
-	public int completeLinesHeuristic(State s) {
-		int[][] field = s.getField();
-		int completeLines = 0;
-
-		for(int i=0; i<field.length; i++) {
-			boolean isComplete = true;
-			for(int j=0; j<field[0].length; j++) {
-				if(field[i][j] == 0) {
-					isComplete = false;
-					break;
-				}
-			}
-			if(isComplete) {
-				completeLines++;
-			}
-		}
-		return completeLines;
-	}
-
-	public int bumpinessHeuristic(State s){
-		int[][] fields = s.getField();
+	public int bumpinessHeuristic(int[][] field){
 		int bumpiness = 0;
 		boolean debug = false;
 
-		int[] depths = getColumnHeights(s);
+		int[] depths = getColumnHeights(field);
 		
 		if(debug){
 			System.out.println("Start Bumpiness Heuristic Printout");
-			for(int i=0; i<fields.length; i++){
-				for(int j=0; j<fields[i].length; j++){
-					System.out.print(fields[i][j]);
+			for(int i=0; i<field.length; i++){
+				for(int j=0; j<field[i].length; j++){
+					System.out.print(field[i][j]);
 				}
 				System.out.println();
 			}
@@ -64,17 +77,16 @@ public class PlayerSkeleton {
 			//System.out.print(depths[i] + " ");
 			bumpiness += Math.abs(depths[i] - depths[i+1]);
 		}
-		if(debug){
-			System.out.println("Bumpiness is " + bumpiness);
-			System.out.println("End Depth Score Printout");	
-		}
+//		if(debug){
+//			System.out.println("Bumpiness is " + bumpiness);
+//			System.out.println("End Depth Score Printout");	
+//		}
 
 		return bumpiness;
 	}
 
 	//returns an int array of column heights from column 0 to column 9, left to right
-	public int[] getColumnHeights(State s) {
-		int[][] field = s.getField();
+	public int[] getColumnHeights(int[][] field) {
 		int[] heights = new int[field[0].length];
 		int maxHeight = field.length;
 
@@ -91,14 +103,107 @@ public class PlayerSkeleton {
 		return heights;
 	}
 	
+	// Similar logic to makeMove() in State, simulates a single move based on an original state
+	// @param s - original state
+	// @param move - move to simulate
+	// @return field - a duplicated field with the new outcome based on the move
+	public int[][] simulateMove(State s, int[] move) {
+		int[][] originalField = s.getField();
+		int[] originalTop = s.getTop();
+		int[][] pWidth = State.getpWidth();
+		int[][] pHeight = State.getpHeight();
+		int[][][] pBottom = State.getpBottom();
+		int[][][] pTop = State.getpTop();
+		int nextPiece = s.getNextPiece();
+		int turn = s.getTurnNumber();
+		int orient = move[ORIENT];
+		int slot = move[SLOT];
+		
+		// Copy into a new field and top array
+		// Otherwise it will refer to the same field, top arrays for each simulation
+		int[][] field = new int[ROWS][COLS];
+		int[] top = new int[COLS];
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				field[i][j] = originalField[i][j];
+			}
+		}
+		for (int i = 0; i < COLS; i++) {
+			top[i] = originalTop[i];
+		}
+		
+		//height if the first column makes contact
+		int height = top[slot]-pBottom[nextPiece][orient][0];
+		//for each column beyond the first in the piece
+		for(int c = 1; c < pWidth[nextPiece][orient];c++) {
+			height = Math.max(height,top[slot+c]-pBottom[nextPiece][orient][c]);
+		}
+		
+		//check if game ended
+		if(height+pHeight[nextPiece][orient] >= ROWS) {
+			gameEnded = true;
+			return field;
+		}
+
+		
+		//for each column in the piece - fill in the appropriate blocks
+		for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
+			
+			//from bottom to top of brick
+			for(int h = height+pBottom[nextPiece][orient][i]; h < height+pTop[nextPiece][orient][i]; h++) {
+				field[h][i+slot] = turn;
+			}
+		}
+		
+		//adjust top
+		for(int c = 0; c < pWidth[nextPiece][orient]; c++) {
+			top[slot+c]=height+pTop[nextPiece][orient][c];
+		}
+		
+		rowsCleared = 0;
+		
+		//check for full rows - starting at the top
+		for(int r = height+pHeight[nextPiece][orient]-1; r >= height; r--) {
+			//check all columns in the row
+			boolean full = true;
+			for(int c = 0; c < COLS; c++) {
+				if(field[r][c] == 0) {
+					full = false;
+					break;
+				}
+			}
+			//if the row was full - remove it and slide above stuff down
+			if(full) {
+				rowsCleared++;
+				//for each column
+				for(int c = 0; c < COLS; c++) {
+
+					//slide down all bricks
+					for(int i = r; i < top[c]; i++) {
+						field[i][c] = field[i+1][c];
+					}
+					//lower the top
+					top[c]--;
+					while(top[c]>=1 && field[top[c]-1][c]==0)	top[c]--;
+				}
+			}
+		}
+
+		return field;
+	}
+	
 	public static void main(String[] args) {
 		State s = new State();
 		new TFrame(s);
 		PlayerSkeleton p = new PlayerSkeleton();
 		int turnCount = 0;
 		while(!s.hasLost()) {
-			System.out.print("Hello turn num " + turnCount);
+			System.out.println("Hello turn num " + turnCount);
 			turnCount++;
+//			int[][] legalMoves = s.legalMoves();
+//			for (int i = 0; i < legalMoves.length; i++) {
+//				System.out.println(Arrays.toString(legalMoves[i]));
+//			}
 			s.makeMove(p.pickMove(s,s.legalMoves()));
 			s.draw();
 			s.drawNext(0,0);
